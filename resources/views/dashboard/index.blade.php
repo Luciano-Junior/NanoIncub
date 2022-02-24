20458e1f246249aab43447de3c631773e25bd276
Luciano Junior <tec.luciano.ti@gmail.com>
2022-02-24 15:13:48 -0300

    add view dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Dashboard - NanoIncub')
+@section('content')
+    <div class="container-fluid">
+        Teste
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.index');
 });
 Route::get('/login', function () {
     return view('login.login');
