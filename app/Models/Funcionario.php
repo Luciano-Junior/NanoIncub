b998ce58db2f21360bc7d5922780a7884c1346df
Luciano Junior <tec.luciano.ti@gmail.com>
2022-02-24 22:03:03 -0300

    add funcionario model

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    //
+    protected $table = 'funcionario';
+
+    const CREATED_AT = 'data_criacao';
+    const UPDATED_AT = 'data_alteracao';
+
+    protected $fillable = [
+        'nome_completo',
+        'login',
+        'senha',
+        'saldo_atual',
+        'administrador_id',
+    ];
 }
